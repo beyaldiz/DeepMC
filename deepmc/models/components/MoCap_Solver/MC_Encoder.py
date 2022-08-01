67a0fd487d7e89ca40535d57b1cbf4367b1d5198
Baha Eren YALDIZ <beyaldiz17@gmail.com>
2022-08-01 17:35:43 +0900

    Add motion encoder and MS components

--- deepmc/models/components/MoCap_Solver/MC_Encoder.py
+++ deepmc/models/components/MoCap_Solver/MC_Encoder.py
@@ -9,6 +9,7 @@ import numpy as np
 
 
 from deepmc.models.components.MoCap_Solver.skeleton import SkeletonUnpool, SkeletonPool, SkeletonConv, find_neighbor, SkeletonLinear, build_edge_topology
+from deepmc.utils.MoCap_Solver.utils import _dict_marker, _dict_joints
 
 
 class TS_enc(nn.Module):
@@ -211,11 +212,12 @@ class MO_enc(nn.Module):
         skeleton_dist: int = 2,
         extra_conv: int = 0,
     ):
-        super(MO_Enc, self).__init__()
-        self.topologies = [topology]
+        super(MO_enc, self).__init__()
+        edges = build_edge_topology(topology, torch.zeros((len(topology), 3)))
+        self.topologies = [edges]
         self.channel_base = [4]
         self.channel_list = []
-        self.edge_num = [len(topology) + 1]
+        self.edge_num = [len(edges) + 1]
         self.pooling_list = []
         self.layers = nn.ModuleList()
         self.convs = []
@@ -268,7 +270,7 @@ class MO_enc(nn.Module):
         return input
 
 
-class MO_Dec(nn.Module):
+class MO_dec(nn.Module):
     def __init__(
         self,
         topology: list = [0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 12, 13, 14, 16, 17, 18, 19, 20, 21],
@@ -277,10 +279,11 @@ class MO_Dec(nn.Module):
         skeleton_dist: int = 2,
         extra_conv: int = 0,
     ):
-        super(MO_Dec, self).__init__()
+        super(MO_dec, self).__init__()
+        edges = build_edge_topology(topology, torch.zeros((len(topology), 3)))
         self.layers = nn.ModuleList()
         self.unpools = nn.ModuleList()
-        enc = MO_Enc(topology=topology, num_layers=num_layers, kernel_size=kernel_size,
+        enc = MO_enc(topology=topology, num_layers=num_layers, kernel_size=kernel_size,
                         skeleton_dist=skeleton_dist,extra_conv=extra_conv)
         self.convs = []
 
@@ -325,3 +328,230 @@ class MO_Dec(nn.Module):
         input = input[:, :-1, :]
 
         return input
+
+
+class Marker_enc(nn.Module):
+    def __init__(
+        self,
+        num_markers: int = 56,
+        num_joints: int = 24
+    ):
+        super(Marker_enc, self).__init__()
+        # Hard coded
+        FRAMENUM = 64
+        self.gamma_para = 0.001
+        self.HUBER_DELTA = 200
+        self.lambda_para = np.ones((num_markers, 3))
+        self.lambda_jt_para = np.ones((num_joints, 3))
+        self.head_idx = [_dict_marker["ARIEL"], _dict_marker["LBHD"], _dict_marker["LFHD"], _dict_marker["RFHD"],
+                    _dict_marker["RBHD"]]
+        self.head_jt_idx = [_dict_joints["Head"]]
+        self.shoulder_idx = [_dict_marker["LTSH"], _dict_marker["LBSH"], _dict_marker["LFSH"], _dict_marker["RTSH"],
+                        _dict_marker["RFSH"], _dict_marker["RBSH"]]
+        self.shoulder_jt_idx = [_dict_joints['L_Collar'], _dict_joints['R_Collar'], _dict_joints['L_Shoulder'], _dict_joints['R_Shoulder']]
+        self.arm_idx = [
+            _dict_marker["LUPA"], _dict_marker["LIEL"], _dict_marker["LELB"], _dict_marker["LWRE"],
+            _dict_marker["RUPA"], _dict_marker["RIEL"], _dict_marker["RELB"], _dict_marker["RWRE"]
+        ]
+        self.arm_jt_idx = [_dict_joints['L_Elbow'], _dict_joints['R_Elbow']]
+        self.wrist_hand_idx = [
+            _dict_marker["LOWR"], _dict_marker["LIWR"], _dict_marker["LIHAND"], _dict_marker["LOHAND"],
+            _dict_marker["ROWR"], _dict_marker["RIWR"], _dict_marker["RIHAND"], _dict_marker["ROHAND"]
+        ]
+        self.wrist_hand_jt_idx = [_dict_joints['L_Wrist'], _dict_joints['R_Wrist'], _dict_joints['L_Hand'], _dict_joints['R_Hand']]
+
+        self.torso_idx = [
+            _dict_marker["CLAV"], _dict_marker["STRN"], _dict_marker["C7"], _dict_marker["T10"], _dict_marker["L4"],
+            _dict_marker["LMWT"], _dict_marker["LFWT"], _dict_marker["LBWT"],
+            _dict_marker["RMWT"], _dict_marker["RFWT"], _dict_marker["RBWT"]
+        ]
+
+        self.torso_jt_idx = [_dict_joints['Pelvis'],_dict_joints['Spine1'], _dict_joints['Spine2'], _dict_joints['Spine3'], _dict_joints['L_Hip'], _dict_joints['R_Hip'], _dict_joints['Neck']]
+        self.thigh_idx = [
+            _dict_marker["LKNI"], _dict_marker["LKNE"], _dict_marker["LHIP"], _dict_marker["LSHN"],
+            _dict_marker["RKNI"], _dict_marker["RKNE"], _dict_marker["RHIP"], _dict_marker["RSHN"]
+        ]
+        self.thigh_jt_idx = [_dict_joints['L_Knee'], _dict_joints['R_Knee']]
+
+        self.foots_idx = [
+            _dict_marker["LANK"], _dict_marker["LHEL"], _dict_marker["LMT1"], _dict_marker["LTOE"],
+            _dict_marker["LMT5"],
+            _dict_marker["RANK"], _dict_marker["RHEL"], _dict_marker["RMT1"], _dict_marker["RTOE"], _dict_marker["RMT5"]
+        ]
+        self.foots_jt_idx = [_dict_joints['L_Ankle'], _dict_joints['R_Ankle'], _dict_joints['L_Foot'], _dict_joints['R_Foot']]
+
+        self.lambda_para[[self.head_idx]] = self.lambda_para[[self.head_idx]] * 10  # head
+        self.lambda_para[[self.shoulder_idx]] = self.lambda_para[[self.shoulder_idx]] * 5  # shoulder
+        self.lambda_para[[self.arm_idx]] = self.lambda_para[[self.arm_idx]] * 8  # arm
+        self.lambda_para[[self.wrist_hand_idx]] = self.lambda_para[[self.wrist_hand_idx]] * 10  # wrist
+        self.lambda_para[[self.torso_idx]] = self.lambda_para[[self.torso_idx]] * 5  # torso
+        self.lambda_para[[self.thigh_idx]] = self.lambda_para[[self.thigh_idx]] * 8  # thigh
+        self.lambda_para[[self.foots_idx]] = self.lambda_para[[self.foots_idx]] * 10  # foots
+
+        self.lambda_jt_para[[self.head_jt_idx]] = self.lambda_jt_para[[self.head_jt_idx]] * 10  # head
+        self.lambda_jt_para[[self.shoulder_jt_idx]] = self.lambda_jt_para[[self.shoulder_jt_idx]] * 5  # shoulder
+        self.lambda_jt_para[[self.arm_jt_idx]] = self.lambda_jt_para[[self.arm_jt_idx]] * 8  # arm
+        self.lambda_jt_para[[self.wrist_hand_jt_idx]] = self.lambda_jt_para[[self.wrist_hand_jt_idx]] * 10  # wrist
+        self.lambda_jt_para[[self.torso_jt_idx]] = self.lambda_jt_para[[self.torso_jt_idx]] * 5  # torso
+        self.lambda_jt_para[[self.thigh_jt_idx]] = self.lambda_jt_para[[self.thigh_jt_idx]] * 8  # thigh
+        self.lambda_jt_para[[self.foots_jt_idx]] = self.lambda_jt_para[[self.foots_jt_idx]] * 10  # foots
+        self.joints_nums = num_joints
+        self.marker_nums = num_markers
+        self.layers = nn.ModuleList()
+        self.normLayers = nn.ModuleList()
+        self.actLayers = nn.ModuleList()
+        self.prevLayer = nn.Sequential(Flatten(),
+                                       Linear(FRAMENUM * num_markers * 3, 2048))
+        self.layers.append(Linear(2048, 2048))
+        self.normLayers.append(BatchNorm1d(2048))
+        self.actLayers.append(LeakyReLU(0.5))
+        self.layers.append(Linear(2048, 2048))
+        self.normLayers.append(BatchNorm1d(2048))
+        self.actLayers.append(LeakyReLU(0.5))
+        self.layers.append(Linear(2048, 2048))
+        self.normLayers.append(BatchNorm1d(2048))
+        self.actLayers.append(LeakyReLU(0.5))
+
+        self.actLayers.append(LeakyReLU(0.5))
+        self.layers.append(Linear(2048, 2048))
+        self.actLayers.append(LeakyReLU(0.5))
+
+    def forward(self, input):
+        input1 = self.prevLayer(input)
+        x0 = self.normLayers[0](input1)
+        x0 = self.actLayers[0](x0)
+        x = self.layers[0](x0)
+        m0 = x + x0
+        x1 = self.normLayers[1](m0)
+        x1 = self.actLayers[1](x1)
+        x = self.layers[1](x1)
+        m1 = x + x1
+        x2 = self.normLayers[2](m1)
+        x2 = self.actLayers[2](x2)
+        x = self.layers[2](x2)
+        m2 = x + x2
+        x3 = self.normLayers[2](m2)
+        x3 = self.actLayers[3](x3)
+        x3 = self.layers[3](x3)
+        x = self.actLayers[4](x3)
+        return x
+
+
+class Marker_dec(nn.Module):
+    def __init__(self):
+        super(Marker_dec, self).__init__()
+        self.configlayers = nn.ModuleList()
+        self.confignormLayers = nn.ModuleList()
+        self.configactLayers = nn.ModuleList()
+        self.mdlayers = nn.ModuleList()
+        self.mdnormLayers = nn.ModuleList()
+        self.mdactLayers = nn.ModuleList()
+        self.offsetlayers = nn.ModuleList()
+        self.offsetnormLayers = nn.ModuleList()
+        self.offsetactLayers = nn.ModuleList()
+        self.transLayers = nn.ModuleList()
+        self.transactLayers = nn.ModuleList()
+
+        self.configlayers.append(Linear(2048, 2048))
+        self.confignormLayers.append(BatchNorm1d(2048))
+        self.configactLayers.append(LeakyReLU(0.5))
+
+        self.configlayers.append(Linear(2048, 2048))
+        self.confignormLayers.append(BatchNorm1d(2048))
+        self.configactLayers.append(LeakyReLU(0.5))
+
+        self.configactLayers.append(LeakyReLU(0.5))
+        self.configlayers.append(Linear(2048, 1024))
+
+        self.mdlayers.append(Linear(2048, 2048))
+        self.mdnormLayers.append(BatchNorm1d(2048))
+        self.mdactLayers.append(LeakyReLU(0.5))
+
+        self.mdlayers.append(Linear(2048, 2048))
+        self.mdnormLayers.append(BatchNorm1d(2048))
+        self.mdactLayers.append(LeakyReLU(0.5))
+
+
+
+        self.mdlayers.append(Linear(2048, 2048))
+        self.mdnormLayers.append(BatchNorm1d(2048))
+        self.mdactLayers.append(LeakyReLU(0.5))
+
+        self.mdactLayers.append(LeakyReLU(0.5))
+        self.mdlayers.append(Linear(2048, 2048))
+
+
+        self.offsetlayers.append(Linear(2048, 2048))
+        self.offsetnormLayers.append(BatchNorm1d(2048))
+        self.offsetactLayers.append(LeakyReLU(0.5))
+
+        self.offsetlayers.append(Linear(2048, 2048))
+        self.offsetnormLayers.append(BatchNorm1d(2048))
+        self.offsetactLayers.append(LeakyReLU(0.5))
+
+        self.offsetlayers.append(Linear(2048, 2048))
+        self.offsetnormLayers.append(BatchNorm1d(2048))
+        self.offsetactLayers.append(LeakyReLU(0.5))
+
+        self.offsetactLayers.append(LeakyReLU(0.5))
+        self.offsetlayers.append(Linear(2048, 168))
+
+
+    def forward(self, input):
+        ######marker config#######################
+        x_vector = self.confignormLayers[0](input)
+        x_vector = self.configactLayers[0](x_vector)
+        x_vector1 = self.configlayers[0](x_vector)
+        x_vector = x_vector + x_vector1
+
+        x_vector = self.confignormLayers[1](x_vector)
+        x_vector = self.configactLayers[1](x_vector)
+        x_vector1 = self.configlayers[1](x_vector)
+        x_vector = x_vector + x_vector1
+
+        x_vector = self.configactLayers[2](x_vector)
+        mrk_config_x_o = self.configlayers[2](x_vector)
+        mrk_config_x_o = mrk_config_x_o.reshape([mrk_config_x_o.shape[0], 1024])
+
+        ######motion data#############################
+        md_vector = self.mdnormLayers[0](input)
+        md_vector = self.mdactLayers[0](md_vector)
+        md_vector1 = self.mdlayers[0](md_vector)
+        md_vector = md_vector + md_vector1
+
+        md_vector = self.mdnormLayers[1](md_vector)
+        md_vector = self.mdactLayers[1](md_vector)
+        md_vector1 = self.mdlayers[1](md_vector)
+        md_vector = md_vector + md_vector1
+
+        md_vector = self.mdnormLayers[2](md_vector)
+        md_vector = self.mdactLayers[2](md_vector)
+        md_vector1 = self.mdlayers[2](md_vector)
+        md_vector = md_vector + md_vector1
+
+        md_vector5 = self.mdactLayers[3](md_vector)
+        md_x_o = self.mdlayers[3](md_vector5)
+        md_x_o = md_x_o.reshape([md_x_o.shape[0], 2048])
+
+
+        ######offset data#############################
+        offset_vector = self.offsetnormLayers[0](input)
+        offset_vector = self.offsetactLayers[0](offset_vector)
+        offset_vector1 = self.offsetlayers[0](offset_vector)
+        offset_vector = offset_vector + offset_vector1
+
+        offset_vector = self.offsetnormLayers[1](offset_vector)
+        offset_vector = self.offsetactLayers[1](offset_vector)
+        offset_vector1 = self.offsetlayers[1](offset_vector)
+        offset_vector = offset_vector + offset_vector1
+
+        offset_vector = self.offsetnormLayers[2](offset_vector)
+        offset_vector = self.offsetactLayers[2](offset_vector)
+        offset_vector1 = self.offsetlayers[2](offset_vector)
+        offset_vector = offset_vector + offset_vector1
+
+        offset_vector = self.offsetactLayers[3](offset_vector)
+        offset_x_o = self.offsetlayers[3](offset_vector)
+        offset_x_o = offset_x_o.reshape([offset_x_o.shape[0], 168])
+
+        return mrk_config_x_o, md_x_o, offset_x_o
--- deepmc/models/components/MoCap_Solver/__init__.py
+++ deepmc/models/components/MoCap_Solver/__init__.py
@@ -1,1 +1,1 @@
-from deepmc.models.components.MoCap_Solver.MC_Encoder import TS_enc, TS_dec, MC_enc, MC_dec, MO_enc, MO_dec
+from deepmc.models.components.MoCap_Solver.MC_Encoder import TS_enc, TS_dec, MC_enc, MC_dec, MO_enc, MO_dec, Marker_enc, Marker_dec
